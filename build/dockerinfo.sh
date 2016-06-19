9881f53e90086f4d01b6ac96f702355602a8b32a
Aryeh Leib Taurog <git@aryehleib.com>
2016-06-19 22:01:48 +0300

    Generate random container name

--- build/dockerinfo.sh
+++ build/dockerinfo.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 . build/version
 INFOVERSION=$(python info/setup.py --version)
+CONTAINER=$(</dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
 if docker run \
         -v $PWD/info:/build \
-        --name infodist \
+        --name $CONTAINER \
         altaurog/python:$TAG \
         pip install /build
 then
     docker commit \
         -a "Aryeh Leib Taurog <python@aryehleib.com>" \
-        infodist altaurog/dockerinfo:$INFOVERSION
+        $CONTAINER altaurog/dockerinfo:$INFOVERSION
     docker commit \
         -a "Aryeh Leib Taurog <python@aryehleib.com>" \
-        infodist altaurog/dockerinfo:latest
-    docker rm infodist
+        $CONTAINER altaurog/dockerinfo:latest
+    docker rm $CONTAINER
 fi
