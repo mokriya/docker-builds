5328688209945b83893a0dd3dd5e424867870c2a
Aryeh Leib Taurog <git@aryehleib.com>
2016-07-21 21:56:49 +0300

    fix: minimize root activity
    Run build as nobody instead of root, and set final *.deb ownership

--- build/buildpython.sh
+++ build/buildpython.sh
@@ -2,12 +2,13 @@
 . build/version
 TEMPDIR=$(mktemp -d -p packages/)
 BUILDCONTAINER=$(</dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+ARGS="`id -u`:`id -g` $PKGNAME $PYVERSION $RELEASE"
 if docker run \
         -v $PWD/$TEMPDIR:/build \
         --tmpfs /tmp:rw,exec,size=1g \
         --name $BUILDCONTAINER \
-        altaurog/pybuild:$DEBVERSION $PKGNAME $PYVERSION $RELEASE
+        altaurog/pybuild:$DEBVERSION $ARGS
 then
-    docker run --rm -v $PWD:/m debian mv /m/$TEMPDIR/$PYTHONDEB /m/packages
+    mv $TEMPDIR/$PYTHONDEB packages
 fi
 rm -rf $TEMPDIR
--- build/python.sh
+++ build/python.sh
@@ -2,7 +2,7 @@
 . build/version
 ls packages/$PYTHONDEB || exit 1
 TEMPDIR=$(mktemp -d -p packages/)
-docker run --rm -v $PWD:/m debian ln /m/packages/$PYTHONDEB /m/$TEMPDIR/
+ln packages/$PYTHONDEB $TEMPDIR/
 M4="m4 -P -D debversion=$DEBVERSION -D pythondeb=$PYTHONDEB"
 
 $M4 python/Dockerfile.m4 > $TEMPDIR/Dockerfile
