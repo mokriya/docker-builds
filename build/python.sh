44d9ba6308b1a993ef590fe2d49acb9ad1d34052
Aryeh Leib Taurog <git@aryehleib.com>
2016-09-05 22:38:59 +0300

    chore: add latest tag to base images

--- build/python.sh
+++ build/python.sh
@@ -10,10 +10,12 @@ if docker run \
         -v $PWD/packages/$PYTHONDEB:$BUILD/$PYTHONDEB \
         altaurog/debbase:$DEBVERSION $BUILD/install.sh $PYTHONDEB
 then
+    REPO=altaurog/python
     docker commit \
         -c "ENV PATH /venv/bin:/usr/local/bin:/usr/bin:/bin" \
         -a "Aryeh Leib Taurog <python@aryehleib.com>" \
-        $BUILDCONTAINER altaurog/python:$TAG
+        $BUILDCONTAINER $REPO:$TAG
+    docker tag $(docker images -q $REPO:$TAG) $REPO:latest
 fi
 
 BUILDCONTAINER=$(</dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
@@ -22,9 +24,11 @@ if docker run \
         -v $PWD/wheelbuild:$BUILD \
         altaurog/python:$TAG $BUILD/{install.sh,buildwheels.sh}
 then
+    REPO=altaurog/wheelbuild
     docker commit \
         -c 'ENV PATH /venv/bin:/usr/local/bin:/usr/bin:/bin' \
         -c 'ENTRYPOINT ["/venv/bin/buildwheels.sh"]' \
         -a 'Aryeh Leib Taurog <python@aryehleib.com>' \
-        $BUILDCONTAINER altaurog/wheelbuild:$TAG
+        $BUILDCONTAINER $REPO:$TAG
+    docker tag $(docker images -q $REPO:$TAG) $REPO:latest
 fi
